cb83d05836033ea194002ae3b204bf5152013cfc
RicardoFLD <63625002+RicardoFLD@users.noreply.github.com>
2023-04-13 11:45:40 -0600

    -----

--- Destinos.sql
+++ Destinos.sql
@@ -1,22 +1,8 @@
 USE [DestTurist]
 GO
 
-/****** Object:  Table [dbo].[Destinos]    Script Date: 13/4/2023 01:23:54 ******/
-SET ANSI_NULLS ON
-GO
-
-SET QUOTED_IDENTIFIER ON
-GO
-
-CREATE TABLE [dbo].[Destinos](
-	[foto] [varchar](500) NULL,
-	[Nombre] [varchar](50) NOT NULL,
-	[Descripcion] [varchar](500) NULL,
-	[Precio] [int] NULL,
- CONSTRAINT [PK_Destinos] PRIMARY KEY CLUSTERED 
-(
-	[Nombre] ASC
-)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
-) ON [PRIMARY]
+/****** Object:  Table [dbo].[Destinos]    Script Date: 13/4/2023 11:45:20 ******/
+IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[Destinos]') AND type in (N'U'))
+DROP TABLE [dbo].[Destinos]
 GO
 
